85f60ca6ec28910a770fda0c6e881e3d81844f80
Grzegorz Wirtek <grzegorz.wirtek@wp.pl>
2021-06-18 15:39:58 +0200

    adding Loader element and class

--- js/GetApi.js
+++ js/GetApi.js
@@ -1,4 +1,5 @@
 import { viewWeather } from './ViewWeather.js';
+import { loader } from './Loader.js';
 
 const KEY_1 = '5a';
 const KEY_2 = 45869;
@@ -6,15 +7,17 @@ const KEY_3 = 687534;
 const KEY_4 = 'dd0e6fbf210e812854b3c91c';
 
 export class GetApi{
-    getFromApi(city){     
+    getFromApi(city){   
+        loader.startLoader();  
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pl&appid=${KEY_1 + (KEY_2+KEY_3).toString() + KEY_4}`;
         fetch(url)
             .then(e => e.json())
             .then(info => {
+                loader.stopLoader();
                 return viewWeather.createElements(
                     {
                         name: info.name,
-                        ont: '1.5rem',
+                        font: '1.5rem',
                         description: info.weather[0].description,
                         iconUrl: `https://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`,    
                         temp: info.main.temp,
@@ -24,23 +27,15 @@ export class GetApi{
                         pressure: info.main.pressure,
                         humidity: info.main.humidity,
                         windSpeed: info.wind.speed
-                    } 
-                ) 
+                    }
+                );
             })
             .catch(err =>{
+                loader.stopLoader();
                 return viewWeather.createElements(
                     {
                         name: `Twoja przeglądarka blokuje dostęp do zasobów ze strony: "https://openweathermap.org"`,
                         font: '0.8rem',
-                        description: 0,
-                        iconUrl: null,    
-                        temp: 0,
-                        tempFeelsLike: 0,
-                        tempMin: 0,
-                        tempMax: 0,
-                        pressure: 0,
-                        humidity: 0,
-                        windSpeed: 0
                     } 
                 ) 
             })
